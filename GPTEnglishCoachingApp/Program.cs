bd97415f7b2bd3e032052d25f32586f2707038bb
07JP27 <jynpei.t@gmail.com>
2024-03-31 01:08:36 +0900

    add mudblazor

--- GPTEnglishCoachingApp/Program.cs
+++ GPTEnglishCoachingApp/Program.cs
@@ -1,10 +1,13 @@
 using GPTEnglishCoachingApp.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
