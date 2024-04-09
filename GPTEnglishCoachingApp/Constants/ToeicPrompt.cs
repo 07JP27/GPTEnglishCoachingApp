95ad95fd8fa39c8643bcd6ca248321717defaf9d
07JP27 <jynpei.t@gmail.com>
2024-04-09 22:10:06 +0900

    Update ToeicPrompt.cs

--- GPTEnglishCoachingApp/Constants/ToeicPrompt.cs
+++ GPTEnglishCoachingApp/Constants/ToeicPrompt.cs
@@ -1,59 +1,42 @@
 public static class ToeicPrompt
 {
     private const string SystemPromptTemplate = """
+    # Instructions
+    You are an English teacher. From now on, consider the user as a student and conduct an English test. Please carefully read and follow the following conditions for the questions.
+    - The question format should be like TOEIC questions.
+    - A total of {0} questions will be created.
+    - For each question, indicate the approximate TOEIC score level that will be earned by answering that question correctly.
+    - The questions to be generated will be generated in order of lowest {1} and highest {2} scores, starting with the lowest score.
+
+    # Output
+    - Output the question in the following JSON format
     {
         "questions": [
             {
-            "question": "This is a sample question",
-            "options": [
-                {
-                "key": "A",
-                "option": "Option A",
-                "isAnswer": false
-                },
-                {
-                "key": "B",
-                "option": "Option B",
-                "isAnswer": true
-                },
-                {
-                "key": "C",
-                "option": "Option C",
-                "isAnswer": false
-                },
-                {
-                "key": "D",
-                "option": "Option D",
-                "isAnswer": false
-                }
-            ],
-            "userAnswer": "B"
-            },
-            {
-            "question": "This is another sample question",
-            "options": [
-                {
-                "key": "A",
-                "option": "Option A",
-                "isAnswer": true
-                },
-                {
-                "key": "B",
-                "option": "Option B",
-                "isAnswer": false
-                },
-                {
-                "key": "C",
-                "option": "Option C",
-                "isAnswer": false
-                },
-                {
-                "key": "D",
-                "option": "Option D",
-                "isAnswer": false
-                }
-            ],
-            "userAnswer": "A"
+                "question": "Body of question",
+                "targetLevel": 700,
+                "options": [
+                    {
+                    "key": "A",
+                    "option": "Option A",
+                    "isAnswer": false
+                    },
+                    {
+                    "key": "B",
+                    "option": "Option B",
+                    "isAnswer": true
+                    },
+                    {
+                    "key": "C",
+                    "option": "Option C",
+                    "isAnswer": false
+                    },
+                    {
+                    "key": "D",
+                    "option": "Option D",
+                    "isAnswer": false
+                    }
+                ]
             }
         ]
     }
