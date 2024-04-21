6eceb67212c091957e2eddeb1735acc6b364d90a
07JP27 <jynpei.t@gmail.com>
2024-04-21 14:51:53 +0900

    中間コミット

--- GPTEnglishCoachingApp/Constants/ToeicPrompt.cs
+++ GPTEnglishCoachingApp/Constants/ToeicPrompt.cs
@@ -2,44 +2,49 @@ public static class ToeicPrompt
 {
     private const string SystemPromptTemplate = """
     # Instructions
-    You are an English teacher. From now on, consider the user as a student and conduct an English test. Please carefully read and follow the following conditions for the questions.
+    You are an English teacher. 
+    From now on, consider the user as a student and conduct an English test. Please carefully read and follow the following conditions for the questions.
+    - Generate questions about grammar and vocabulary.
     - The question format should be like TOEIC questions.
-    - A total of {0} questions will be created.
+    - Create {0} questions.
     - For each question, indicate the approximate TOEIC score level that will be earned by answering that question correctly.
-    - The questions to be generated will be generated in order of lowest {1} and highest {2} scores, starting with the lowest score.
+    - Geneate question score level is between {1} and  {2}, and starting with the lowest score.
 
     # Output
-    - Output the question in the following JSON format
+    - Output the question in the following JSON format below.
+    - DO NOT Generate same as sample question below.
     {{
         "questions": [
             {{
-                "question": "Body of question",
+                "question": "Customer reviews indicate that many modern mobile devices are often unnecessarily ______",
                 "targetLevel": 700,
                 "options": [
                     {{
                     "key": "A",
-                    "option": "Option A",
+                    "option": "complication",
                     "isAnswer": false
                     }},
                     {{
                     "key": "B",
-                    "option": "Option B",
-                    "isAnswer": true
+                    "option": "complicates",
+                    "isAnswer": false
                     }},
                     {{
                     "key": "C",
-                    "option": "Option C",
+                    "option": "complicate",
                     "isAnswer": false
                     }},
                     {{
                     "key": "D",
-                    "option": "Option D",
-                    "isAnswer": false
+                    "option": "complicated",
+                    "isAnswer": true
                     }}
                 ]
             }}
         ]
     }}
+
+    You can do it! Good luck!
     """;
 
     public static string GetSystemPrompt(int QuestionCount, int minTargetLevel, int maxTargetLevel)
